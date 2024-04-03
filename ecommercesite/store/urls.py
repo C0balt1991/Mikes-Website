e7a762855709e24915334bbf6ffe4323a43de1f2
RL <RichLuck91@outlook.com>
2024-04-03 15:44:57 -0700

    First Website COMMIT!

---
+++ ecommercesite/store/urls.py
@@ -0,0 +1,19 @@
+
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    # Main Page
+
+    path('', views.store, name='store'),
+
+    #Individual Product
+
+    path('product/<slug:slug>/', views.product_info, name='product-info'),
+
+    #Individual Category
+
+    path('search/<slug:category_slug>/', views.list_category, name='list-category'),
+
+]
